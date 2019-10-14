a265f61de8b40b36ab5832b84b1f12a0aa619da1
Eduardo Argollo <eargollo@gmail.com>
2019-10-13 19:13:12 -0700

    Fix tests

--- sshcode.go
+++ sshcode.go
@@ -463,7 +463,7 @@ func syncUserSettings(sshFlags string, host string, back bool) error {
 		return err
 	}
 
-	const remoteSettingsDir = ".local/share/code-server/User/"
+	const remoteSettingsDir = "~/.local/share/code-server/User/"
 
 	var (
 		src  = localConfDir + "/"
@@ -489,7 +489,7 @@ func syncExtensions(sshFlags string, host string, back bool) error {
 		return err
 	}
 
-	const remoteExtensionsDir = ".local/share/code-server/extensions/"
+	const remoteExtensionsDir = "~/.local/share/code-server/extensions/"
 
 	var (
 		src  = localExtensionsDir + "/"
